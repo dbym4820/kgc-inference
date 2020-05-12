87ef568dd81d362cbc866c759293370c7a9ca9f3
Tomoki Aburatani <aburatanitomoki@gmail.com>
2020-05-13 05:10:23 +0900

    一段落

--- inf_samp.pl
+++ inf_samp.pl
@@ -10,6 +10,9 @@
 conc_kgc_ontology(Concept, Result) :- 
     concat_atom(['http://kgc.knowledge-graph.jp/ontology/kgc.owl', Concept], '#', Result).
 
+conc_rdf_ontology(Concept, Result) :-
+    concat_atom(['http://www.w3.org/1999/02/22-rdf-syntax-ns', Concept], '#', Result).
+
 %% 原文データを取得・これだけ構造が違ったので別定義
 findSceneSource(SceneNum, Source) :-
     conc_kgc_ontology('source', SourceAtom),
@@ -19,7 +22,7 @@ findSceneSource(SceneNum, Source) :-
 findSceneConcept(SceneNum, Pred, Concept) :-
     conc_kgc_ontology(Pred, PredicateAtom),
     rdf(SceneNum, PredicateAtom, Concept), !.
-findSceneConcept(_, _, "NULL") :- !.
+findSceneConcept(_, _, 'null') :- !.
 
 %% 特定シーンに対して，付与されている概念を見つけ出して一元化
 findSceneInfo(SceneNum, Source, Subject, Predicate, What, Where, To) :-
@@ -57,31 +60,77 @@ getAllSceneAboveTime(_読み進めているシーン番号, Scenes) :-
 	    Scenes).
 
 %===================================================%
+%        ここから特定シーンに出てくる情報の解析機
+%===================================================%
+%% 特定シーンに人が出てくるか
+findRelatedConcept(TargetConcept, Pred, Concept) :-
+    conc_rdf_ontology(Pred, PredicateAtom),
+    rdf(TargetConcept, PredicateAtom, Concept), !.
+findRelatedConcept(_, _, 'null') :- !.
+
+getSceneRelatedTypes(SceneNum,
+		     [TypeofSubject, Subject],
+		     [TypeofPredicate, Predicate],
+		     [TypeofWhat, What],
+		     [TypeofWhere, Where],
+		     [TypeofTo, To]) :-
+    getInfoFromSceneNum(SceneNum, scene(_, _, Subject, Predicate, What, Where, To)),
+    findRelatedConcept(Subject, 'type', TypeofSubject),
+    findRelatedConcept(Predicate, 'type', TypeofPredicate),
+    findRelatedConcept(What, 'type', TypeofWhat),
+    findRelatedConcept(Where, 'type', TypeofWhere),
+    findRelatedConcept(To, 'type', TypeofTo).
+
+findConcept(SceneNum, Concept, SubConcepts) :-
+    conc_kgc_ontology(Concept, ConceptAtom),
+    bagof(Subject,
+	  getSceneRelatedTypes(SceneNum,
+			       [ConceptAtom | [Subject]],
+			       _, _, _, _),
+	  [SubConcepts]).
+
+%% Subjectのときのみだけど，そのシーンに人がいるかどうか，
+%% いるとしたら誰がいるのか，を捉えられるように成ったやつ
+isAppearPerson(SceneNum, Person) :-
+    findConcept(SceneNum, 'Person', Person).
+
+%===================================================%
 %        ここからオントロジー -> Rule/Factの定義（これは，すなわち仮説とほぼ同じ？）
 %===================================================%
 %% load_trick_ontology(OntologyFilePath).
-%% trick(トリック名, )
-%% trick('一人二役トリック', )
+%% このTrickのルールが，オントロジーファイルをLoadして，動的に作り上げることになる述語群
+%% 
+trick('犯人が被害者に化けるトリック') :-
+    dead(X), a.
+
+trick('〜トリック') :-
+    aaaaa.
 
 
-%% trick('犯人が被害者に化けるトリック') :-
-%%     a.
-
-%% trick('〜トリック') :-
-%%     aaaaa.
-
-
-%% available_world(_読み進めているシーン番号, Person, Trick) :-
-%%     a.
-
-%% available_reason(available_world(_読み進めているシーン番号, Person, Trick), Reason) :-
-%%     a.
+%===================================================%
+%        ここから，Feedback生成器
+%===================================================%
+%% 学習者が，特定シーンまで見たとき，検討することが望ましい可能世界
+available_world(_読み進めているシーン番号, [Person, Trick]) :-
+    getAllSceneAboveTime(_読み進めているシーン番号, Scenes),
+    %%  ここで，Scenesから得た情報をもとに，トリックに関係するFactをAssertする？とにかく，次のTrickが成立・非成立するように事実を追加する処理を行う
+    trick(Trick).
 
 %% %% hypothesis(トリック行為者, トリック). %% 誰が，何をした可能性があるのか・ロイロットが１人二役（によって県議を逃れようとしている可能性）
-%% %% こういう形態になってたら，仮説として提示できんじゃね？ってこと
+%% %% ↓こういう形態になってたら，仮説として提示できんじゃね？ってこと
 %% %% hypothesis(persoin(roylotte), trick('犯人が被害者に化けるトリック')).
-%% hypothesis_inference(_読み進めているシーン番号, Hypothesis) :-
-%%     bagof(hypothesis(Person, Trick),
-%% 	  available_world(_読み進めているシーン番号, Person, Trick),
-%% 	 Hypothesis).
+hypothesis_inference(_読み進めているシーン番号, Hypothesis) :-
+    bagof(hypothesis(Person, Trick),
+	  available_world(_読み進めているシーン番号, [Person, Trick]),
+	 Hypothesis).
 	
+
+%%%%%%%%% 説明性を以下に高めてフィードバックできるか（推論プロセスの情報を以下に取得するか）が課題
+
+
+%%%＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝%%%
+%%%    残った課題は，３つ
+%%%    ・トリックの中身を（１事例でもいいので）定義する
+%%%    ・特定のシーンまで読解したときの，そこまでの情報をFactとして，PrologにAssertする仕組み（維持例を出すための，ほんの一部で良いと思う）
+%%%    ・説明性を保ったフィードバック（仮説）の提示処理
+%%%＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝%%%
